09e7b1d115b92d7396ddfb5921015e218c64906d
Mark Strendin <mark.strendin@gmail.com>
2021-02-05 19:29:08 -0600

    Ridlium Infused Crystal instead of Ridlium Crystal

--- src/main/java/ridleydyne/ridlium/item/RidliumDust.java
+++ src/main/java/ridleydyne/ridlium/item/RidliumDust.java
@@ -3,17 +3,6 @@ package ridleydyne.ridlium.item;
 import net.minecraft.item.Item;
 import ridleydyne.ridlium.Ridlium;
 
-/*
-import java.util.List;
-import javax.annotation.Nullable;
-import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraft.util.text.ITextComponent;
-*/
-
 public class RidliumDust extends Item {
     public RidliumDust() {
         super(new Item.Properties()
@@ -22,12 +11,4 @@ public class RidliumDust extends Item {
         );
     }
 
-    /*
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
-        super.addInformation(stack, world, tooltip, flag);
-
-        //tooltip.add(new TranslationTextComponent("").appendString("").mergeStyle(TextFormatting.GRAY));
-    }
-    */
 }
